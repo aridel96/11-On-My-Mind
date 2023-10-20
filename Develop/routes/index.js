6ff0ae59e520de37f0c981e0a38b7d270d3e0b82
aridel96 <adelgado.162@gmail.com>
2023-10-20 01:18:23 -0400

    Worked on server.js and routes

---
+++ Develop/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const notesRouter = require('./notes');      // Import router for notes.js
+
+const app = express();
+
+app.use('/notes', notesRouter);             // For request that start with a /notes look at the notesRouter
+
+module.exports = app;
+
+
---
+++ Develop/routes/notes.js
@@ -0,0 +1,6 @@
+const notes = require('express').Router();
+
+// const { v4: uuidv4 } = require('uuid');
+// const fs = require('fs');
+
+module.exports = notes;
