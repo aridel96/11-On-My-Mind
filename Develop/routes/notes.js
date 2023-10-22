b611abda2f40f01a5c5007b433b3af92c93ee29d
aridel96 <adelgado.162@gmail.com>
2023-10-22 02:38:09 -0400

    Worked on post route handler in routes/notes.js - adding user Note to json file

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -6,14 +6,14 @@ const addNote = fs.createWriteStream('./db/db.json', { flags: 'a'})      // Crea
 notes.get('/', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {                  // if there's an error console logs the error
-            console.error(err)
+            console.error(err);
         }
         res.json(JSON.parse(data))      // Since readFile returns a string we convert to an object and then back to json. We attach this to the response
     })
 })
 
 notes.post('/', (req, res) => {
-    const {title, text} = req.body      //Object deconstructuring - same as doing title = req.body.title
+    const {title, text} = req.body;      //Object deconstructuring - same as doing title = req.body.title
     
     if (title && text) {                // Checks to see that both title and text exist
         const newNote = {
@@ -22,8 +22,17 @@ notes.post('/', (req, res) => {
             note_id: uuidv4()           // calls uuidv4 to create a universally unique identifier for each note posted
         }
 
-        addNote.write(JSON.stringify(newNote));         // writes to the db.json file
-        addNote.end()                   // ends the write stream
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {                  // if there's an error console logs the error
+                console.error(err);
+            }
+
+            let savedNotes = JSON.parse(data);
+            savedNotes.push(newNote)                   // Appends the new Note at the end of the array in db.json
+            
+            addNote.write(JSON.stringify(savedNotes));         // writes to the db.json file
+            addNote.end()                   // ends the write stream
+        })
 
         const response = {
             status: 'success',
@@ -36,4 +45,24 @@ notes.post('/', (req, res) => {
     }
 })
 
+// notes.delete('/', (req, res) => {
+
+// })
+
 module.exports = notes;
+
+
+
+
+
+
+
+
+// if (savedNotes === 'undefined' || savedNotes === 'null') {      // Checks to see if the db.json file is empty
+//     let notesArray = [];                                        // If empty then it creates an array and pushes the new Note to the array
+//     notesArray.push(newNote)
+//     addNote.write(JSON.stringify(notesArray));         // writes to the db.json file
+// } else {
+//     savedNotes.push(newNote)                   // Appends the new Note at the end of the array in db.json
+//     addNote.write(JSON.stringify(savedNotes));         
+// }
